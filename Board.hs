5a73090e1d8328804cdb43615b535a18b4eae82f
theLife10 <jdgarcia9@miners.utep.edu>
2019-12-01 00:54:41 -0700

    checking a board and dropping a disc

--- Board.hs
+++ Board.hs
@@ -1,4 +1,4 @@
-module Board(mkBoard, mkPlayer, mkOpponent) where
+module Board(mkBoard, mkPlayer, mkOpponent, dropInSlot) where
 
 import Data.List
 import Data.Array
@@ -12,3 +12,33 @@ mkPlayer = 1
 --oponent 
 mkOpponent :: Int
 mkOpponent = 2
+
+dropInSlot :: [[Int]] -> Int -> Int -> [[Int]]
+dropInSlot bd i p = makeNewColumn bd i newColumn
+    where currentColumn = getColumn bd i;
+          e = getEmptySpot currentColumn;
+          newColumn = insertChip e p currentColumn
+
+getColumn :: [[Int]] -> Int -> [Int]
+getColumn bd i = bd !! i
+
+insertChip i chip (x:xs)
+    | i == 0 = chip:xs
+    | otherwise = x:insertChip (i-1) chip xs
+
+getEmptySpot column = last ( findIndices(==0) column)
+
+makeNewColumn :: [[Int]] -> Int -> [Int] -> [[Int]]
+makeNewColumn bd i newColumn = front ++ [newColumn] ++ end
+    where front = take (i) bd;
+          end = drop(i+1) bd
+
+isSlotOpen :: [[Int]] -> Int -> Bool
+isSlotOpen bd i = elem 0 column
+    where column = bd !! i
+
+numSlot:: [[Int]] -> Int
+numSlot bd = length bd
+
+isFull:: [[Int]] -> Bool
+isFull bd = not ( elem 0 (concat bd))
