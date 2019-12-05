1ae5e3b5ede26f8c7e538ed4799182bb020a2d21
samarjorg <Sftslth99>
2019-12-04 20:31:45 -0700

    Print String Board almost

--- Board.hs
+++ Board.hs
@@ -6,21 +6,27 @@ import Data.Array
 
 mkBoard :: Int -> Int ->[[Int]]
 mkBoard m n = replicate m (replicate n 0)
+
 --player 1
 mkPlayer :: Int
 mkPlayer = 1
---oponent 
+
+--oponent
 mkOpponent :: Int
 mkOpponent = 2
 
+-- Takes in board, column number, and value, puts the value in the board at the column number
 dropInSlot :: [[Int]] -> Int -> Int -> [[Int]]
+--bd is the board, i is the column, p is the player marker
 dropInSlot bd i p = makeNewColumn bd i newColumn
     where currentColumn = getColumn bd i;
-          e = getEmptySpot currentColumn;
+          e = getEmptySpot currentColumn; --e is emptyPosition
           newColumn = insertChip e p currentColumn
 
+--Takes in board, and column number, returns column
 getColumn :: [[Int]] -> Int -> [Int]
 getColumn bd i = bd !! i
+
 
 insertChip i chip (x:xs)
     | i == 0 = chip:xs
@@ -33,12 +39,44 @@ makeNewColumn bd i newColumn = front ++ [newColumn] ++ end
     where front = take (i) bd;
           end = drop(i+1) bd
 
+--Takes in board and column number, returns True or False if the column is not full
 isSlotOpen :: [[Int]] -> Int -> Bool
 isSlotOpen bd i = elem 0 column
     where column = bd !! i
 
+-- Takes in board and returns the amount of columns
 numSlot:: [[Int]] -> Int
 numSlot bd = length bd
 
+--Takes in board and returns the amount of rows
+numRow:: [[Int]] -> Int
+numRow bd = length ( bd !! 0)
+
+--Takes in board, row, and column and returns the tokens
+getToken:: [[Int]] -> Int -> Int -> Int
+getToken bd r c = (bd !! r) !! c
+
+--Takes in board and returns if the board is full (True if its full, False if not)
 isFull:: [[Int]] -> Bool
 isFull bd = not ( elem 0 (concat bd))
+
+
+
+
+boardToStr :: (Int -> String) -> [[Int]] -> Int -> String
+boardToStr playerToChar bd (-1) = ""
+boardToStr playerToChar bd i = val playerToChar (bd !! i) 0 ++ boardToStr playerToChar bd (i-1)
+  where
+    val :: (Int -> String) -> [Int] -> Int -> String
+    val playerToChar slot 7 = ""
+    val playerToChar slot x = playerToChar (slot !! x) ++ val playerToChar slot (x+1)
+
+
+
+
+
+playerToChar :: Int -> String
+playerToChar val
+ | (val == 1) = "O "
+ | (val == 2) = "X "
+ | otherwise = "."
